f1e64078a12689ebb82227ac7ae6e15ac0b424f5
whatabast <89269007+whatabast@users.noreply.github.com>
2026-04-22 22:24:31 -0700

    Add function to create input prompt Create if statement inside to catch invalid entries

--- sketch.js
+++ sketch.js
@@ -1,18 +1,30 @@
+//Create constant for the body element
 const body = document.body;
+// const notValid = document.createElement("div");
+
+
+// Create a function that pops up a propmt for size input
+function boxSize()
+{
+    let size = prompt("Input");
+    if(size > 0 && size <= 100)
+    {
+        console.log("Valid");
+    }
+    else 
+    {
+        alert("Not Valid Size")
+    }
+}
+
+
+// Create a parent for all the other boxes to nest inside
 const main = document.createElement("div");
 main.className="main";
 body.append(main);
 
-function resize(newSize)
-{
-
-}
-
-buttonSize = document.createElement("button");
-body.append(buttonSize);
-buttonSize.textContent = "Create New Grid";
-
-let size = 16; //prompt("How big do you want the grid?");
+// Create a nested for loop that can create an NxN box of any size
+let size = 16;
 
 let wrapper = [];
 let box = [];

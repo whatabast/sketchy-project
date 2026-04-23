a9aac84960ae96fff58bd9df966ebf37183839e4
whatabast <89269007+whatabast@users.noreply.github.com>
2026-04-22 22:31:20 -0700

    Accidentally made a function that create MORE boxes

--- sketch.js
+++ sketch.js
@@ -1,22 +1,5 @@
 //Create constant for the body element
 const body = document.body;
-// const notValid = document.createElement("div");
-
-
-// Create a function that pops up a propmt for size input
-function boxSize()
-{
-    let size = prompt("Input");
-    if(size > 0 && size <= 100)
-    {
-        console.log("Valid");
-    }
-    else 
-    {
-        alert("Not Valid Size")
-    }
-}
-
 
 // Create a parent for all the other boxes to nest inside
 const main = document.createElement("div");
@@ -29,17 +12,37 @@ let size = 16;
 let wrapper = [];
 let box = [];
 
-for(let j=1; j<=size; j++)
-{
-    wrapper[j] = document.createElement("div");
-    wrapper[j].className="wrapper"+j;
-    main.append(wrapper[j]);
-
-    for(let i=1; i<=size; i++)
+function sizing(size){
+    for(let j=1; j<=size; j++)
     {
-        box[i] = document.createElement("div");
-        box[i].className = "box" + i;
-        wrapper[j].append(box[i]);
-        box[i].textContent = "box" + i;
+        wrapper[j] = document.createElement("div");
+        wrapper[j].className="wrapper"+j;
+        main.append(wrapper[j]);
+
+        for(let i=1; i<=size; i++)
+        {
+            box[i] = document.createElement("div");
+            box[i].className = "box" + i;
+            wrapper[j].append(box[i]);
+            box[i].textContent = "box" + i;
+        }
+    }
+}
+sizing(size);
+
+
+// Create a function that pops up a propmt for size input
+function boxSize()
+{
+    let size = prompt("Input");
+    if(size > 0 && size <= 100)
+    {
+        console.log("Valid");
+        sizing(size);
+
+    }
+    else 
+    {
+        alert("Not Valid Size")
     }
 }
